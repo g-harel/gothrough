5d113de28e8546334cee6fb98fbf108a510ebe88
g-harel <gabrielj.harel@gmail.com>
2020-10-29 19:20:15 -0500

    add tests for tag parsing util

--- internal/cases/split_test.go
+++ internal/cases/split_test.go
@@ -15,25 +15,33 @@ func TestSplit(t *testing.T) {
 			Input:    "",
 			Expected: []string{},
 		},
-		"simple double": {
+		"camel simple double": {
 			Input:    "TestCase",
 			Expected: []string{"Test", "Case"},
 		},
-		"simple n-tuple": {
+		"camel simple n-tuple": {
 			Input:    "TestCaseThatIsLongerThanTheOtherOne",
 			Expected: []string{"Test", "Case", "That", "Is", "Longer", "Than", "The", "Other", "One"},
 		},
-		"prefix acronym": {
+		"camel prefix acronym": {
 			Input:    "HTTPTestCase",
 			Expected: []string{"HTTP", "Test", "Case"},
 		},
-		"postfix acronym": {
+		"camel postfix acronym": {
 			Input:    "ServeHTTP",
 			Expected: []string{"Serve", "HTTP"},
 		},
-		"surrounded acronym": {
+		"camel surrounded acronym": {
 			Input:    "AbcABCAbc",
 			Expected: []string{"Abc", "ABC", "Abc"},
+		},
+		"snake simple double": {
+			Input:    "test_case",
+			Expected: []string{"test", "case"},
+		},
+		"mixed camel/snake": {
+			Input:    "AbcDef_xyz123",
+			Expected: []string{"Abc", "Def", "xyz123"},
 		},
 	}
 
@@ -41,11 +49,11 @@ func TestSplit(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			actual := cases.Split(tc.Input)
 			if len(tc.Expected) != len(actual) {
-				t.Fatalf("expected/actual do not match\n%v\n%v", tc.Expected, actual)
+				t.Fatalf("expected/actual do not match\n'%v'\n'%v'", tc.Expected, actual)
 			}
 			for i := range tc.Expected {
 				if len(tc.Expected) != len(actual) || tc.Expected[i] != actual[i] {
-					t.Fatalf("expected/actual do not match\n%v\n%v", tc.Expected, actual)
+					t.Fatalf("expected/actual do not match\n'%v'\n'%v'", tc.Expected, actual)
 				}
 			}
 		})
---
+++ internal/tags/parse_test.go
@@ -0,0 +1,84 @@
+package tags_test
+
+import (
+	"testing"
+
+	"github.com/g-harel/gothrough/internal/tags"
+)
+
+type Expected struct {
+	Words string
+	Tags  map[string][]string
+}
+
+func TestSplit(t *testing.T) {
+	tt := map[string]struct {
+		Input    string
+		Expected Expected
+	}{
+		"empty": {
+			Input:    "",
+			Expected: Expected{},
+		},
+		"trim words": {
+			Input: " abc ",
+			Expected: Expected{
+				Words: "abc",
+			},
+		},
+		"single tags": {
+			Input: "test:true",
+			Expected: Expected{
+				Tags: map[string][]string{"test": {"true"}},
+			},
+		},
+		"multiple tags": {
+			Input: "abc:123 xyz:456",
+			Expected: Expected{
+				Tags: map[string][]string{"abc": {"123"}, "xyz": {"456"}},
+			},
+		},
+		"duplicate tags": {
+			Input: "a:1 a:2",
+			Expected: Expected{
+				Tags: map[string][]string{"a": {"1", "2"}},
+			},
+		},
+		"tags and words": {
+			Input: "test:false abc test:true",
+			Expected: Expected{
+				Words: "abc",
+				Tags:  map[string][]string{"test": {"false", "true"}},
+			},
+		},
+		"multiple colons": {
+			Input: "test:test:test",
+			Expected: Expected{
+				Tags: map[string][]string{"test": {"test:test"}},
+			},
+		},
+		"duplicate values": {
+			Input: "test:a test:a",
+			Expected: Expected{
+				Tags: map[string][]string{"test": {"a"}},
+			},
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			actual := tags.Parse(tc.Input)
+			if tc.Expected.Words != actual.GetWords() {
+				t.Fatalf("expected/actual words do not match\n'%v'\n'%v'", tc.Expected.Words, actual.GetWords())
+			}
+			for tag, values := range tc.Expected.Tags {
+				actualValues := actual.GetTags(tag)
+				for i := range values {
+					if values[i] != actualValues[i] {
+						t.Fatalf("expected/actual tags do not match\n'%v'\n'%v'", values, actualValues)
+					}
+				}
+			}
+		})
+	}
+}
